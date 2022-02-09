2aba4d4d014621858c5993e599b6a0e15fcc4671
Viet Nguyen <vietnguyen2330@gmail.com>
2022-02-08 22:10:44 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -115,7 +115,7 @@ function RenderCampsite({campsite}) {
   )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -133,7 +133,7 @@ function RenderComments({comments, addComment, campsiteId}) {
             </div>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -176,11 +176,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} /> 
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
-                    />
+                    />     
                 </div>
             </div>
         );
